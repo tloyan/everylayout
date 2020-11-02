a52094ae12d489291cda7499970e7afbe2074ae4
thomas loyan <devways@thomass-MacBook-Pro.local>
2020-11-02 16:11:04 +0100

    finish the first component stack

--- src/routes/stack/normal/index.js
+++ src/routes/stack/normal/index.js
@@ -2,12 +2,10 @@ import { h } from 'preact';
 
 import style from './style.css'
 
-export default () => {
-  return (
-    <div className={style.stack}>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
-}
+export default () => (
+  <div className={style.stack}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
