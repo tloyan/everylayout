a768598f5b0efc03ee8251ddf23665206bc798fe
devways <thomas.loyan.dev@gmail.com>
2020-11-02 17:30:49 +0100

    add composition stack recursive

--- src/components/app.js
+++ src/components/app.js
@@ -5,12 +5,14 @@ import { Router } from 'preact-router';
 import Home from '../routes/home';
 
 import StackNormal from '../routes/stack/normal'
+import StackRecursive from '../routes/stack/recursive';
 
 const App = () => (
 	<div id="app">
 		<Router>
 			<Home path="/" />
 			<StackNormal path="/stack/normal" />
+			<StackRecursive path="/stack/recursive" />
 		</Router>
 	</div>
 )
